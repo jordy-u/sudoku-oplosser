7467940f80fb584dd0140b2032f3d5492a43693b
jordy-u <jordyubink@hotmail.nl>
2021-01-10 14:11:07 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# sudoku-oplosser
+Dit is een hobby-project om sudoku's op te lossen. Ik doe dit project omdat ik me verveel.
---
+++ src/nl/jordyu/sudoku/ReadSudokuFile.java
@@ -0,0 +1,25 @@
+package nl.jordyu.sudoku;
+
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.Scanner; // Import the Scanner class to read text files
+
+// Bron: https://www.w3schools.com/java/java_files_read.asp
+
+public class ReadSudokuFile {
+
+    public ReadSudokuFile(String filename) {
+        try {
+            File myObj = new File(filename);
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                System.out.println(data);
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+}
